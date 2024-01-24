2268e5dabb2f01ca77aef36aa9d4c953b61f8f84
mostofa-s-cse <mostofa.s.cse@gmail.com>
2024-01-24 23:25:38 +0600

    login and register done

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\v1\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::post('/auth/register', [UserController::class, 'createUser']);
+// Route::post('/auth/login', [UserController::class, 'loginUser']);
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group([
+    'prefix' => 'v1/auth',
+], function ($router) {
+    Route::post('/register', [UserController::class, 'createUser']); //http://127.0.0.1:8000/api/v1/auth/register with bearer token
+    Route::post('/login', [UserController::class, 'loginUser']); //http://127.0.0.1:8000/api/v1/auth/login with bearer token
 });
+
